b8a66519f21a7a259000cbca6f68c8afe85fb688
Servesh Khandwe <54593871+ServeshKhandwe@users.noreply.github.com>
2023-11-18 20:50:00 +0100

    Update machinelearning.py

--- machinelearning.py
+++ machinelearning.py
@@ -7,6 +7,48 @@ class machinelearning:
     def __init__(self, file_path):
         self.file_path = file_path
         self.recipes_df = self.load_data()
+        self.first=True
+
+    # Initialize or update the user's liked recipes in a session state
+        if 'liked_recipes' not in st.session_state:
+            st.session_state.liked_recipes = set()
+        if 'sample_recipes' not in st.session_state:
+            st.session_state.sample_recipes = self.recipes_df.sample(1)
+        # if 'next_pressed' not in st.session_state:
+        #     st.session_state.next_pressed = False
+
+
+    # Display recipe selection checkboxes
+        # for recipe in st.session_state.sample_recipes.itertuples():
+        #     checkbox_label = f"{recipe.name} (ID: {recipe.id})"
+        #     if st.checkbox(checkbox_label, key=recipe.id):
+        #         st.session_state.liked_recipes.add(recipe.id)
+        #     elif recipe.id in st.session_state.liked_recipes:
+        #         st.session_state.liked_recipes.remove(recipe.id)
+
+        # if st.button("Next"):
+        #     st.session_state.sample_recipes = self.recipes_df.sample(1)
+        #     st.session_state.next_pressed = True
+
+        # if st.button("Submit Feedback") and st.session_state.next_pressed:
+        #     st.write("You have liked these recipes (IDs):", st.session_state.liked_recipes)
+
+        if len(st.session_state.liked_recipes) >= 5:
+                
+            model, tfidf_vectorizer = self.train_model(list(st.session_state.liked_recipes))
+            recommendations = self.get_recommendations(model, tfidf_vectorizer, list(st.session_state.liked_recipes))
+                
+
+            recommended_recipes_array = []
+            for index, row in recommendations.iterrows():
+                recommended_recipe = f"{row['name']} (ID: {row['id']})"
+                st.text(recommended_recipe)
+                recommended_recipes_array.append(recommended_recipe)
+        else:
+            st.write("Please like a total of 5 recipes to train the model.")
+
+        if not st.session_state.next_pressed:
+            st.experimental_rerun()
 
     @st.cache(allow_output_mutation=True)
     def load_data(self):
@@ -32,57 +74,20 @@ class machinelearning:
         recommended_indices = [i for i in recommended_indices if i not in liked_indices]
         return self.recipes_df.iloc[recommended_indices]
 
-    def generateOnereceipe():
-        recipe=load_data()
-        return recipe.sample(1)
-
-def main():
-    file_path = '/Users/serveshkhandwe/Desktop/hacaktumtry/RAW_recipes.csv'  # Or use st.text_input to get user input
-    recommender = RecipeRecommender(file_path)
-
-    st.title("Recipe Recommendation System")
-
-    # Initialize or update the user's liked recipes in a session state
-    if 'liked_recipes' not in st.session_state:
-        st.session_state.liked_recipes = set()
-    if 'sample_recipes' not in st.session_state:
-        st.session_state.sample_recipes = recommender.recipes_df.sample(1)
-    if 'next_pressed' not in st.session_state:
-        st.session_state.next_pressed = False
-
-    st.header("Select recipes you like from these options")
-
-    # Display recipe selection checkboxes
-    for recipe in st.session_state.sample_recipes.itertuples():
-        checkbox_label = f"{recipe.name} (ID: {recipe.id})"
-        if st.checkbox(checkbox_label, key=recipe.id):
-            st.session_state.liked_recipes.add(recipe.id)
-        elif recipe.id in st.session_state.liked_recipes:
-            st.session_state.liked_recipes.remove(recipe.id)
-
-    if st.button("Next"):
-        st.session_state.sample_recipes = recommender.recipes_df.sample(1)
-        st.session_state.next_pressed = True
-
-    if st.button("Submit Feedback") and st.session_state.next_pressed:
-        st.write("You have liked these recipes (IDs):", st.session_state.liked_recipes)
-
-        if len(st.session_state.liked_recipes) >= 5:
-            st.write("Training the model based on your likes...")
-            model, tfidf_vectorizer = recommender.train_model(list(st.session_state.liked_recipes))
-            recommendations = recommender.get_recommendations(model, tfidf_vectorizer, list(st.session_state.liked_recipes))
-            st.write("Recommended Recipes based on your likes:")
-
-            recommended_recipes_array = []
-            for index, row in recommendations.iterrows():
-               recommended_recipe = f"{row['name']} (ID: {row['id']})"
-                st.text(recommended_recipe)
-                recommended_recipes_array.append(recommended_recipe)
+    def generateOnereceipe(self):
+        
+        if self.first==True:
+            recipe=self.load_data()
+            return recipe.sample(n=1).iloc[0].to_dict()
+            
         else:
-            st.write("Please like a total of 5 recipes to train the model.")
+            model, tfidf_vectorizer = self.train_model(list(st.session_state.liked_recipes))
+            recommendations = self.get_recommendations(model, tfidf_vectorizer, list(st.session_state.liked_recipes))
+            for index, row in recommendations.iterrows():
+                recommended_recipe = f"{row['name']} (ID: {row['id']})"
+                return recommended_recipe
+            
+        
+        
 
-    if not st.session_state.next_pressed:
-        st.experimental_rerun()
 
-if __name__ == "__main__":
-    main()
