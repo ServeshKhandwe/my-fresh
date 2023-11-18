1b4a0df34cf34d18d6e5885d3cbe52d14f99f7df
Nandiinii Rawatt <73476764+NandiiniiRawatt@users.noreply.github.com>
2023-11-18 12:03:34 +0100

    Update streamlit_app.py
    added recipies and dictionary

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,38 @@ with right_col:
 # give me a list of recipes!
 # random list at first
 # ranked by recommendation algorithm
+# Sample recipes
+recipes_data = {
+    "Recipe 1": {
+        "Ingredients": ["Chicken", "Broccoli", "Soy Sauce", "Garlic", "Ginger"],
+        "Instructions": "1. Cook chicken in soy sauce.\n2. Add broccoli, garlic, and ginger. Cook until tender.",
+    },
+    "Recipe 2": {
+        "Ingredients": ["Salmon", "Lemon", "Dill", "Olive Oil", "Salt", "Pepper"],
+        "Instructions": "1. Season salmon with salt and pepper.\n2. Squeeze lemon over the salmon. Sprinkle with dill.\n3. Grill or bake until cooked through.",
+    },
+    "Recipe 3": {
+        "Ingredients": ["Pasta", "Tomatoes", "Basil", "Garlic", "Olive Oil"],
+        "Instructions": "1. Cook pasta according to package instructions.\n2. Saute tomatoes, garlic, and basil in olive oil.\n3. Toss cooked pasta with the sauce.",
+    },
+    "Recipe 4": {
+        "Ingredients": ["Quinoa", "Black Beans", "Corn", "Avocado", "Lime", "Cilantro"],
+        "Instructions": "1. Cook quinoa according to package instructions.\n2. Mix quinoa with black beans, corn, diced avocado, lime juice, and chopped cilantro.",
+    },
+    "Recipe 5": {
+        "Ingredients": ["Eggs", "Spinach", "Feta Cheese", "Onion", "Mushrooms"],
+        "Instructions": "1. Saute spinach, mushrooms, and onions.\n2. Beat eggs and pour over the vegetables.\n3. Sprinkle feta cheese on top. Cook until eggs are set.",
+    },
+}
+
+# Display sample recipes
+for recipe_name, recipe_details in recipes_data.items():
+    print(f"Recipe: {recipe_name}")
+    print("Ingredients:", ", ".join(recipe_details["Ingredients"]))
+    print("Instructions:")
+    print(recipe_details["Instructions"])
+    print("\n" + "="*30 + "\n")
+
 
 previous_recipe, current_recipe, next_recipe = st.columns([1, 1.5, 1])
 current_idx = 1
